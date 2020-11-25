e3e8067137d8abb0f634789ea1a14e5ee366b240
Albert Maino III <anthony.maino45@gmail.com>
2020-11-24 22:07:17 -0500

    Updated app.js
    Finished addPhraseToDisplay function.

--- js/app.js
+++ js/app.js
@@ -15,32 +15,45 @@ const phrases = [
 
 
 
-//Makes Overlay Disapear 
+// Makes overlay disapear 
 btnReset.addEventListener( 'click', () => {
     overlay.style.display = 'none';
-});
+})
 
 
-//Generate Random Phrase
+// Generate random phrase
 const getRandomPhrase = (arr) => {
     const randomNumber = Math.floor(Math.random() * phrases.length);
     return phrases[randomNumber];
-};
+}
 
 
-//Calls Random Phrase
+// Calls random phrase
 const randomPhrase = getRandomPhrase(phrases);
 
 
-//Add Phrase to Display
+// Add phrase to display
 const addPhraseToDisplay = (arr) => {
-    //
+    // Loop to create li for each letter in phase
     for(let i = 0; i<arr.length; i++) {
         let li = document.createElement('li');
         li.textContent = arr[i];
         ul.appendChild(li)
-        
+        // Sets classname for phrase charcters
+        if(arr[i] === " "){
+            li.className = 'space';
+        } else {
+            li.className = 'letter';
+        }
     }
-};
+}
 
+
+// Calls phrase to display
 addPhraseToDisplay(randomPhrase);
+
+
+// Checks if letter is in phrase
+const checkLetter = (button) => {
+    
+}
